798da49531152b763478b9c2a487a70bb31dfc0b
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2021-01-15 01:02:25 +0100

    main-sport + vue

--- js/script.js
+++ js/script.js
@@ -2,6 +2,32 @@ var app = new Vue ({
   el: '#root',
   data: {
     logoData:[],
-    headerData:[]
+    headerData:[],
+    activitiesData:[
+      {
+        name: 'Yoga',
+        logo:'img/sponsor-1-2x-200x103.png' ,
+        description: 'Tristique aliquam in nullam habitasse nunc, amet vel consectetur. Nulla in suspendisse dolor, aliquet.',
+        linkDescription: 'Visit Yoga Studio'
+      },
+      {
+        name: 'Fitness Center',
+        logo: 'img/sponsor-2-2x-200x103.png',
+        description: 'Feugiat etiam dui mauris pharetra mauris sed pharetra. Convallis sapien ornare cras faucibus nulla porta dui a.',
+        linkDescription: 'Join Fitness Center'
+      },
+      {
+        name: 'Fitness Club',
+        logo: 'img/sponsor-3-2x-200x103.png',
+        description: 'Nisl at quam adipiscing montes, nec, adipiscing commodo aliquam tincidunt. Ligula in habitant et sagittis imperdiet.',
+        linkDescription: 'Learn about FC'
+      },
+      {
+        name: 'Power Gym',
+        logo: 'img/sponsor-4-2x-200x103.png',
+        description: 'Proin in viverra maecenas et mattis ut nibh enim. Gravida ultricies mi porta lacus. Et posuere velit sed nec eleifend.',
+        linkDescription: 'Visit Power Gym'
+      }
+    ]
   }
 })
